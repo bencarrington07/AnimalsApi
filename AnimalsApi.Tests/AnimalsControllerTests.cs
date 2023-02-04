071ead8560c5cfb17a9cc98e9982b4c3d5570533
Ben Carrington <ben.carrington07@gmail.com>
2023-02-03 21:28:41 -0700

    Adding Test Project a

---
+++ AnimalsApi.Tests/AnimalsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using AnimalsApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AnimalsApi.Tests;
+
+[TestClass]
+public class AnimalsControllerTests
+{
+    private Mock<ILogger> _mockLogger;
+    private AnimalsController _animalsController;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _mockLogger = new Mock<ILogger>();
+        _animalsController = new AnimalsController(_mockLogger.Object);
+    }
+
+    [TestMethod]
+    public void AnimalsGetReturnsCorrectly()
+    {
+        var apiCall = _animalsController.Get();
+        Assert.IsNotNull(apiCall);
+    }
+}
